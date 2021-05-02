b0d3e703b57411e66cbbf84287a8cfdaa1e085c2
abhi020290 <abhi020290@gmail.com>
2021-05-02 14:15:55 +0530

    ADDING COMITS

--- order-services/src/main/java/com/springbootcloud/orderservices/OrderResource.java
+++ order-services/src/main/java/com/springbootcloud/orderservices/OrderResource.java
@@ -1,12 +1,14 @@
 package com.springbootcloud.orderservices;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/order")
+@RefreshScope
 public class OrderResource {
 
     @Value("${config.message}")
